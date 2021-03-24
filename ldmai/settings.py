926dff22246e5f54256e54f2bc44266428e1f356
David Fan <fand01@udayton.edu>
2021-03-23 22:10:48 -0400

    Added allowed host using env variables

--- ldmai/settings.py
+++ ldmai/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -27,6 +28,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# add allowed host through environment variable
+if 'HOST' in os.environ:
+    ALLOWED_HOSTS.append(os.environ['HOST'])
 
 # Application definition
 
