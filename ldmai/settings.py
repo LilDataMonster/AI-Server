18371914b973d96511d4ce96f943a910eda02f73
David Fan <David.Fan@udri.udayton.edu>
2021-09-23 21:21:23 -0400

    Updated weight checking and checking

--- ldmai/settings.py
+++ ldmai/settings.py
@@ -11,8 +11,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 import os
-import urllib.request
+import logging
 from pathlib import Path
+from tqdm import tqdm
+import requests
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,6 +28,42 @@ SECRET_KEY = '7v%gq+_xhes2vfryf5+x0ovz$6hd*&dp6u#1%tl@cfyl4zl$4h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+LOGGING = {
+    'version': 1,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.FileHandler',
+            'filename': 'file.log',
+            'formatter': 'simple'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    }
+}
+
+if DEBUG:
+    # make all loggers use the console.
+    for _logger in LOGGING['loggers']:
+        LOGGING['loggers'][_logger]['handlers'] = ['console']
 
 ALLOWED_HOSTS = []
 
@@ -132,9 +170,28 @@ STATIC_URL = '/static/'
 FILE_UPLOAD_HANDLERS = ['django.core.files.uploadhandler.TemporaryFileUploadHandler',]
 
 # check and download weights
+logger = logging.getLogger("settings")
 lego_path = os.environ['LEGO_WEIGHTS_PATH'] if 'LEGO_WEIGHTS_PATH' in os.environ else os.path.join(BASE_DIR, 'api', 'data', 'weights')
 lego_file = os.environ['LEGO_WEIGHTS_FILE'] if 'LEGO_WEIGHTS_FILE' in os.environ else 'mask_rcnn_lego_0040.h5'
-lego_weights_path = os.path.join(lego_path, lego_file)
-if not Path(lego_weights_path).is_file():
-    default_download_location = 'https://github.com/LilDataMonster/Lego-CNN/releases/latest/download/mask_rcnn_lego_0040.h5'
-    urllib.request.urlretrieve(default_download_location, lego_weights_path)
+lego_weights_path = Path(os.path.join(lego_path, lego_file))
+
+# download and check size
+default_download_location = 'https://github.com/LilDataMonster/Lego-CNN/releases/latest/download/mask_rcnn_lego_0040.h5'
+response = requests.get(default_download_location, stream=True)
+total_size_in_bytes = int(response.headers.get('content-length', 0))
+if not lego_weights_path.is_file() or lego_weights_path.stat().st_size != total_size_in_bytes:
+    logger.info(f'Lego weights not found/partially downloaded... re-downloading weights...')
+    print(f'Lego weights not found/partially downloaded... re-downloading weights...')
+
+    # Streaming, so we can iterate over the response.
+    block_size = 1024  # 1 Kibibyte
+    progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True)
+    with open(lego_weights_path, 'wb') as file:
+        for data in response.iter_content(block_size):
+            progress_bar.update(len(data))
+            file.write(data)
+    progress_bar.close()
+    if total_size_in_bytes != 0 and progress_bar.n != total_size_in_bytes:
+        logger.error("Downloading bytes is not expected!")
+
+
